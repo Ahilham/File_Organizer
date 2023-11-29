8c7e465fd1406a08265040bbb6376a5f46654984
Ahilham <Ahilham@users.noreply.github.com>
2023-11-29 18:34:32 +0800

    Added a file select GUI

--- py_files/main_file.py
+++ py_files/main_file.py
@@ -1,12 +1,22 @@
 import os
+import sys
 import shutil
+import easygui
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 from File_Organizer_class import FileOrganizer
 
+def get_directory():
+    return easygui.diropenbox(title = "Select a directory to organize")
+
 if __name__ == "__main__":
-    path = input("Enter path that needs to be organized:")
-    event_handler = FileOrganizer()
+    path = get_directory()
+
+    if path is None:
+        print("No directory selected.")
+        exit()
+    else:
+        event_handler = FileOrganizer()
 
     for file_name in os.listdir(path):
         file_path = os.path.join(path, file_name)
@@ -15,6 +25,7 @@ if __name__ == "__main__":
     observer = Observer()
     observer.schedule(event_handler, path, recursive = False)
     observer.start()
+    print("Enter Ctrl + C to end.")
 
     try:
         while True:
