c01672cdbb2919f632bf5d9017bc89e2f8d2c233
Ahilham <Ahilham@users.noreply.github.com>
2023-12-01 16:10:28 +0800

    Resolve changes

--- File_Organizer_class.py
+++ File_Organizer_class.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import shutil
 from watchdog.events import FileSystemEventHandler
@@ -20,37 +19,3 @@ class FileOrganizer(FileSystemEventHandler):
         self.organize_file(event.src_path)
 
             
-=======
-import os
-import shutil
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
-
-class FileOrganizer(FileSystemEventHandler):
-    # def on_created(self, event):
-    #     if event.is_directory:
-    #         return
-        
-    #     file_path = event.src_path
-    #     file_type = file_path.split('.')[-1].lower()
-
-    #     destination_directory = f"./{file_type}_files"
-
-    #     os.makedirs(destination_directory, exist_ok = True)
-    #     shutil.move(file_path, os.path.join(destination_directory, os.path.basename(file_path)))
-
-    def organize_file(self, path, file_path):
-        if os.path.isfile(file_path):
-            file_type = file_path.split('.')[-1].lower()
-            destination_directory = f"{path}/{file_type}_files"
-            os.makedirs(destination_directory, exist_ok = True)
-            shutil.move(file_path, os.path.join(destination_directory, os.path.basename(file_path)))
-
-    def on_created(self, event):
-        self.organize_file(event.src_path)
-
-    def on_modified(self, event):
-        self.organize_file(event.src_path)
-
-            
->>>>>>> 845c3d08770ab4c99dfafd625c41491c1bb831e8
